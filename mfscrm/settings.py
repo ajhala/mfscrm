b0f13a2dc7841c1c2b2404ac0a782079d0af4882
ajhala <89611859+ajhala@users.noreply.github.com>
2021-10-10 22:54:52 -0500

    ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-)3$gz9cm7i=0c9a)*k+%$hy(v6sr#-@kuquxyoct2cl(y05dkn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
